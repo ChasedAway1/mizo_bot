f420930e202c8e9305d7a1614214760f5dcd623a
Chased Away <erdemdzh@gmail.com>
2026-06-15 16:48:30 +0800

    Добавлен общий вопрос о порядке приобретения земельного участка

--- handlers/common_handlers.py
+++ handlers/common_handlers.py
@@ -1,8 +1,8 @@
-from keyboards.keyboards import get_back_keyboard
+from keyboards import get_back_keyboard
 
 def register_common_handlers(bot, db):
-
+    
     @bot.on.message(payload={"cmd": "common"})
     async def common(message):
-        text = db.get_common_text()
+        text = db.get_common_question()
         await message.answer(text, keyboard=get_back_keyboard())
